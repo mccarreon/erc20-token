f261dc1a1e4dd31cdfe7beda5a03528e31b1c699
Matt Carreon <mattc.carreon@gmail.com>
2021-06-02 12:02:16 -0700

    fixing transferFrom

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -48,11 +48,11 @@ contract DappToken {
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][_to]);
+        require(_value <= allowance[_from][msg.sender]);
 
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
-        allowance[_from][_to] -= _value;
+        allowance[_from][msg.sender] -= _value;
 
         emit Transfer(_from, _to, _value);
         
