ea66469c2e6b540e90f81dc548dc65e451e919c3
Matt Carreon <mattc.carreon@gmail.com>
2021-06-02 23:28:07 -0700

    buying tokens

--- contracts/DappTokenSale.sol
+++ contracts/DappTokenSale.sol
@@ -7,10 +7,33 @@ contract DappTokenSale {
     address admin;
     DappToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
+    
+    event Sell(
+        address indexed _buyer,
+        uint256 _amount
+    );
 
     constructor(DappToken _tokenContract, uint256 _tokenPrice) public {
         admin = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
+    }
+
+    // multiply
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == multiply(tokenPrice, _numberOfTokens));
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+        
+        // keep track of tokensSold
+        tokensSold += _numberOfTokens;
+
+        // trigger sell event
+        emit Sell(msg.sender, _numberOfTokens);
     }
 }
--- test/DappTokenSale.js
+++ test/DappTokenSale.js
@@ -8,27 +8,75 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 contract('DappTokenSale', (accounts) => {
-  let contractInstance;
-  // token price in wei, equal to 0.001 ETH
-  const tokenPrice = 1000000000000000;
-  
+  let [admin, buyer] = accounts;
+  let tokenSaleInstance;
+  let tokenInstance;
+  const tokenPrice = 1000000000000000; // token price in wei, equal to 0.001 ETH
+  const tokensAvailable = 750000;
+
   beforeEach(async () => {
-    const tokenInstance = await DappToken.new(1000000);
-    contractInstance = await DappTokenSale.new(tokenInstance.address, tokenPrice);
+    tokenInstance = await DappToken.new(1000000);
+    tokenSaleInstance = await DappTokenSale.new(tokenInstance.address, tokenPrice);
+    tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, { from: admin });
   })
 
   context('with intialization of contract', async () => {
     it('has contract address', async () => {
-      const address = await contractInstance.address;
+      const address = await tokenSaleInstance.address;
       expect(address).to.not.equal(0x0, 'expected address to not be 0x0');
     })
     it('has token sale contract address', async () => {
-      const address = await contractInstance.tokenContract();
+      const address = await tokenSaleInstance.tokenContract();
       expect(address).to.not.equal(0x0, 'expected token address to not be 0x0')
     })
     it('sets token price correctly', async () => {
-      const price = await contractInstance.tokenPrice();
+      const price = await tokenSaleInstance.tokenPrice();
       expect(price.toNumber()).to.equal(tokenPrice, 'expected contract token price to equal tokenPrice');
+    })
+  })
+
+  context('with buying tokens', async () => {
+    it('sells correct number of tokens', async () => {
+      const numberOfTokens = 10;
+      const value = numberOfTokens * tokenPrice;
+
+      tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: value });
+      const amountSold = await tokenSaleInstance.tokensSold();
+
+      expect(amountSold.toNumber()).to.equal(numberOfTokens, 'expected number of tokens sold to be 10');
+    })
+    it('triggers sell event', async () => {
+      const numberOfTokens = 10;
+      const value = numberOfTokens * tokenPrice;
+
+      const receipt = await tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: value })
+
+      expect(receipt.logs.length).to.equal(1, 'expected one triggered event');
+      expect(receipt.logs[0].event).to.equal('Sell', 'expected event to be an Sell');
+      expect(receipt.logs[0].args._buyer).to.equal(buyer, 'expected _buyer address to be buyer');
+      expect(receipt.logs[0].args._amount.toNumber()).to.equal(numberOfTokens, 'expected _amount to equal numberOfTokens');
+    })
+    it('correct amount of eth is used for purchase', async () => {
+      await expect(tokenSaleInstance.buyTokens(10, {from: buyer, value: 1})).to.be.rejectedWith('revert');
+    })
+    it('contract has enough tokens to sell', async () => {
+      const numberOfTokens = 800000;
+      const value = numberOfTokens * tokenPrice;
+
+      await expect(tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: value})).to.be.rejectedWith('revert');
+    })
+    it('tokens are transferred correctly', async () => {
+      let contractBalance;
+      let buyerBalance;
+      const numberOfTokens = 10;
+      const value = numberOfTokens * tokenPrice;
+      
+      tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: value });
+      contractBalance = await tokenInstance.balanceOf(tokenSaleInstance.address);
+      buyerBalance = await tokenInstance.balanceOf(buyer);
+
+      expect(contractBalance.toNumber()).to.equal(tokensAvailable - numberOfTokens, 'expected balance of address to be tokensAvailable-numberOfTokens (750000-10)');
+      expect(buyerBalance.toNumber()).to.equal(numberOfTokens, 'expected buyer to have 10 tokens');
     })
   })
 })
